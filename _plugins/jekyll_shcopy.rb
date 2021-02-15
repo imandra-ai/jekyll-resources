92a781f1a7f6e6a768835259d1dd02bb84eda8ec
Dave Aitken <dave.aitken@gmail.com>
2021-02-15 16:02:45 +0000

    update shcopy plugin: include button counter from rule-synth version

--- _plugins/jekyll_shcopy.rb
+++ _plugins/jekyll_shcopy.rb
@@ -3,23 +3,25 @@
 module Jekyll
 	class ShCopyBlock < Liquid::Block
 
-		
+		@@buttonCounter = 0
 
 		def render(context)
 			@baseurl = Jekyll.configuration({})['baseurl']
 			@text = super.strip
+			@bc = @@buttonCounter
+			@@buttonCounter = @bc + 1
 			"<div class=\"terminal\">
 			<div class=\"terminal__copy-ready\">
 			  <div class=\"terminal__copy-ready--top\"><span class=\"terminal__copy-msg\">Ready!</span></div>
 			  <div class=\"terminal__copy-ready--bottom\"></div>
 			</div>
 			<code>
-			<div class=\"highlight\"><pre><span class=\"o\">#{@text}</span></pre></div>		
+			<div class=\"highlight\"><pre><span class=\"o\">#{@text}</span></pre></div>
 			</code>
-			<div id=\"copy-button-id\" class=\"terminal__copy-cover\">
+			<div id=\"copy-button-id#{@bc}\" class=\"terminal__copy-cover\">
 			  <img src=\"#{@baseurl}/jekyll-resources/assets/img/copy.svg\">
 			</div>
-		  	</div><script>document.getElementById('copy-button-id').addEventListener('click', handleCopyClick);</script>
+		  	</div><script>document.getElementById('copy-button-id#{@bc}').addEventListener('click', handleCopyClick);</script>
 		  	"
 		end
 	end
